99fa29a343320e7f5f0b5195aadee7738dce7b91
imvickykumar999 <imvickykumar999@gmail.com>
2021-09-03 19:36:49 +0530

    email otp

--- app.py
+++ app.py
@@ -15,8 +15,60 @@ except Exception as e:
 
 app = Flask(__name__)
 
+# https://myaccount.google.com/u/3/lesssecureapps?pli=1&rapt=AEjHL4MpjjYh8Z-01vJ5GRsQXICYQsXHG0PweSjWenlbAJfes6qNKbHKs_CfCVh0d5qUO58qFeeB0sYCbA3GANLf-965469dVA
+
+@app.route("/vicksmail")
+def vicksmail():
+    return render_template("mailsent.html", sent='no')
+
+@app.route("/mail_sent", methods=['POST'])
+def mail_sent():
+    import smtplib
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+    from email.mime.base import MIMEBase
+    from email import encoders
+
+    fromaddr = "sagar.sws2000@gmail.com"
+    toaddr = request.form['credentials']
+
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+
+    msg['Subject'] = "Vicks OTP"
+    import random
+    otp = str(random.randint(1000,9999))
+    # otp = '7639'
+
+    file1 = open("otp.txt", "w")
+    file1.write(otp)
+    file1.close()
+
+    body = f'''\
+<html>
+  <head>Vicks OTP</head>
+  <body>
+    <p>Hi !<br>
+       How are you?<br>
+       Here is the OTP = {otp} you wanted.
+    </p>
+  </body>
+</html>
+    '''
+    msg.attach(MIMEText(body, 'html'))
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(fromaddr, "pythonsagarvicky")
+
+    text = msg.as_string()
+    s.sendmail(fromaddr, toaddr, text)
+    s.quit()
+    return render_template("mailsent.html", sent='yes')
+
 @app.route("/")
-def yourquotes():
+def vicksbase():
     from vicks import crud
     obj1 = crud.vicks('@Hey_Vicks')
 
@@ -27,39 +79,49 @@ def yourquotes():
 
     data = obj1.pull()
     print('------------------------->', data)
-    return render_template("yourquotes.html",
+    return render_template("vicksbase.html",
                            data = data,
                            )
 
 
-@app.route('/converted_yourquotes', methods=['POST'])
-def converted_yourquotes():
+@app.route('/converted_vicksbase', methods=['POST'])
+def converted_vicksbase():
     from vicks import crud
 
     credentials = request.form['credentials']
+    person = request.form['person']
+    otp = request.form['otp']
+
     if credentials != '@Hey_Vicks':
         return render_template("404.html", message = 'Wrong Credentials')
 
-    person = request.form['person']
+    f=open("otp.txt",'r')
+    getotp = f.read().strip()
+    f = open("otp.txt", "w")
+    f.write('-')
+    f.close()
 
-    if person == '':
-        obj1 = crud.vicks(credentials)
+    if otp == getotp and otp != '-':
+        if person == '':
+            obj1 = crud.vicks(credentials)
+        else:
+            obj1 = crud.vicks(credentials, child = person)
+
+        message = f'''
+        {request.form['message']}
+        '''
+        if message == '':
+            obj1.push()
+        else:
+            obj1.push(message)
+
+        data = obj1.pull()
+        print('------------------------->', data)
+        return render_template("vicksbase.html",
+                               data = data,
+                               )
     else:
-        obj1 = crud.vicks(credentials, child = person)
-
-    message = f'''
-    {request.form['message']}
-    '''
-    if message == '':
-        obj1.push()
-    else:
-        obj1.push(message)
-
-    data = obj1.pull()
-    print('------------------------->', data)
-    return render_template("yourquotes.html",
-                           data = data,
-                           )
+        return render_template("404.html", message = 'Wrong OTP')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/src/helpdesk.js
+++ static/src/helpdesk.js
@@ -1,63 +1,13 @@
-.video-container {
-    position: relative;
-    padding-bottom: 56.25%; /* 16:9 */
-    height: 0;
-    }
 
-.video-container iframe {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    }
+// <script type="text/javascript" src="resources/help desk.js"></script>
 
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 
-.card-img-top {
-  object-fit: cover;
-  object-position: center;
-  /* height: 650px;
-  width: 540px; */
-}
-
-.jumbotron-fluid {
-    background: url("../static/screenshots/back.jpg") center center / cover no-repeat;
-}
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 25px;
-  background-image: url("../static/screenshots/jumbo.jpg");
-  padding: 30px;
-  line-height: 1.6;
-}
-
-h1 {
-  /* text-align: center; */
-  margin-bottom: 30px;
-  border-bottom: 1px #ccc solid;
-}
-
-h3 {
-  margin-top: 20px;
-}
-
-input[type="text"] {
-  border: 1px #ccc solid;
-  width: 300px;
-  padding: 4px;
-  height: 35px;
-  margin-top: 20px;
-}
-
-.card {
-  margin: 20px 0;
-  border: #ccc 1px solid;
-  padding: 20px
-  object-fit: cover;
-}
+(function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/61255aee649e0a0a5cd2c266/1fdsumdpa';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+})();
